c4ecedbc5934744646dc5effd0b459e56800d2e0
Abby Waddell <wabby.404@gmail.com>
2024-08-21 15:45:08 -0400

    Got functionality working

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,10 @@ import { ref } from "vue";
 // when an operator is added, a space will be put before it in the input,
 // and the operator will go into operator
 // when another number is typed after, it will go into numTwo
-const inputString = ref("");
-const numOne = ref(null);
-const numTwo = ref(null);
+const numOne = ref("");
+const numTwo = ref("");
 const operator = ref("");
+const result = ref("");
 
 const add = (a, b) => {
   return a + b;
@@ -39,32 +39,73 @@ const multiple = (a, b) => {
 };
 
 const computeResult = () => {
-  let finalNumber = 100;
-  bothNumbers.value = finalNumber;
+  let num = parseInt(numOne.value);
+  let nom = parseInt(numTwo.value);
+
+  console.log(num);
+  console.log(nom);
+
+  console.log(numOne.value);
+  console.log(numTwo.value);
+
+  result.value = num + nom;
+
+  numOne.value = "";
+  numTwo.value = "";
+  operator.value = "";
 };
 
-const changeInput = () => {};
+const changeInput = (input) => {
+  // numOne, numTwo and operator will reflect the input,
+  // numbers first typed will go into numOne
+  // when an operator is added, a space will be put before it in the input,
+  // and the operator will go into operator
+  // when another number is typed after, it will go into numTwo
+
+  if (typeof input !== "number") {
+    operator.value = input;
+  } else if (operator.value === "") {
+    numOne.value = numOne.value.toString() + input.toString();
+  } else {
+    numTwo.value = numTwo.value.toString() + input.toString();
+  }
+
+  // if (operator.value === "") {
+  //   numOne.value = numOne.value.toString() + input.toString();
+  // } else if (typeof input !== "number") {
+  //   operator.value = input;
+  // } else {
+  //   numTwo.value = numTwo.value.toString() + input.toString();
+  // }
+  console.log(numOne.value);
+  console.log(numTwo.value);
+  console.log(operator.value);
+};
 </script>
 
 <template>
+  <h1>
+    {{
+      `${numOne === null ? "__" : numOne} ${operator} ${
+        numTwo === null ? "__" : numTwo
+      }`
+    }}
+  </h1>
+  <h2>{{ result }}</h2>
   <ol>
     <li v-for="index in 10" :key="index">
-      <button type="text" @click="bothNumbers = `${bothNumbers}${index}`">
+      <button type="text" @click="changeInput(index)">
         {{ index }}
       </button>
       <!-- <button type="text" @click="inputNumber(index)">{{ index }}</button> -->
     </li>
   </ol>
-  <button type="text" @click="bothNumbers = `${bothNumbers} + `">+</button>
+  <button type="text" @click="changeInput('+')">+</button>
   <button type="text">-</button>
   <button type="text">*</button>
   <button type="text">/</button>
-  <form>
-    <!-- <form @submit.prevent="computeResult"></form> -->
-    <input v-model="bothNumbers" type="text" />
-
-    <button type="submit">></button>
-  </form>
+  <br />
+  <button type="text" @click="computeResult">CONSULT COMPUTRON</button>
 </template>
 
 <style scoped></style>
